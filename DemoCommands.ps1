ec346e678dfe5ba66eebd7fdc0987cea8c1c8fcf
Ned Bellavance <nbellavance@anexinet.com>
2018-06-10 09:53:47 -0400

    More variable examples

--- DemoCommands.ps1
+++ DemoCommands.ps1
@@ -13,6 +13,29 @@ New-Alias -Name azd -Value New-AzureRmResourceGroupDeployment
 #Hello World Demo
 azd -Name "Hello-World" -ResourceGroupName $ResourceGroup -TemplateFile .\HelloWorld.json
 
+#Parameter Demo
+azd -Name "Parameter-Example" -ResourceGroupName $ResourceGroup -TemplateFile .\ParameterExamples.json
+
+#Create a Parameter Object
+$templateParameters = @{
+    stringParam = "My string"
+    intParam = 4
+    arrayParam = @(1,2,3,4)
+}
+
+azd -Name "Parameter-Example" -ResourceGroupName $ResourceGroup -TemplateFile .\ParameterExamples.json `
+    -TemplateParameterObject $templateParameters
+
+$templateParameters = @{
+    stringParam = "option 2"
+    intParam = 3
+    arrayParam = @(1,2,3,4)
+}
+
+azd -Name "Parameter-Example" -ResourceGroupName $ResourceGroup -TemplateFile .\ParameterExamples.json `
+    -TemplateParameterObject $templateParameters
+
+
 #Cleanup after yourself, don't be a SLOB
 Remove-AzureRmResourceGroup -Name $ResourceGroup -Force:$true
 
